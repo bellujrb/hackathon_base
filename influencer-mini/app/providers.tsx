656955d332495e937723ebd8e644fefedbb0b927
bellujrb <98968823+bellujrb@users.noreply.github.com>
2025-07-26 20:37:31 -0300

    Wrap children in CampaignProvider within Providers component to manage campaign context

--- influencer-mini/app/providers.tsx
+++ influencer-mini/app/providers.tsx
@@ -3,6 +3,7 @@
 import { type ReactNode } from "react";
 import { base } from "wagmi/chains";
 import { MiniKitProvider } from "@coinbase/onchainkit/minikit";
+import { CampaignProvider } from "./contexts/CampaignContext";
 
 export function Providers(props: { children: ReactNode }) {
   return (
@@ -18,7 +19,9 @@ export function Providers(props: { children: ReactNode }) {
         },
       }}
     >
-      {props.children}
+      <CampaignProvider>
+        {props.children}
+      </CampaignProvider>
     </MiniKitProvider>
   );
 }
